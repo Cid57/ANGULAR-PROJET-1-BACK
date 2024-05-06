446ad91f945af71dec17e99ec80126d20db58f97
Cindy Singer <cindy_singer913@msn.com>
2024-05-06 13:39:05 +0200

    modif

--- header-init.php
+++ header-init.php
@@ -2,9 +2,8 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, GET, DELETE, OPTIONS");
-
 
 // Répondre immédiatement aux requêtes OPTIONS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
